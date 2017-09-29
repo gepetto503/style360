9f6b1c5811db5ee57d8921647340ad5a727edd38
Brendan Hanna <gepetto503@gmail.com>
2017-09-29 15:46:21 -0700

    background image with filter

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="background">
+          <div className="layer"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
